7716b88362bf6ce20756f158c2826f4e0b89db66
zhanghua-069 <13815005969@163.com>
2020-12-20 23:34:31 +0800

    新建Nacos作为配置中心模块

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/fleexy/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.fleexy.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope// 实现配置自动更新
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${local.nacos.config}")
+    private String localNacosConfig;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+    @GetMapping("/config/nacos")
+    public String getNacosConfig() {
+        return localNacosConfig;
+    }
+}
